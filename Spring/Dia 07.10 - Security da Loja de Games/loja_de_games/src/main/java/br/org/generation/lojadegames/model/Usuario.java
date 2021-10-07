79827d44eb33d543a85366002d74557b58d00d3f
unknown <joserobertonobregavieira@hotmail.com>
2021-10-07 11:03:27 -0300

    Adicionando exercício

---
+++ Spring/Dia 07.10 - Security da Loja de Games/loja_de_games/src/main/java/br/org/generation/lojadegames/model/Usuario.java
@@ -0,0 +1,71 @@
+package br.org.generation.lojadegames.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull(message = "O campo Nome é obrigatório!.")
+	private String nome;
+	
+	@NotNull(message = "O campo Usuário é obrigatório!.")
+	private String usuario;
+	
+	@NotBlank(message = "O campo Senha é obrigatório!.")
+	private String senha;
+	
+	@OneToMany(mappedBy="usuario", cascade = CascadeType.REMOVE)	//CascadeType de ALL para REMOVE
+	@JsonIgnoreProperties("usuario")
+	private List<Produto> produto;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+}
